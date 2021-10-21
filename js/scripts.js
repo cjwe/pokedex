e7af2413f5ea1377609a501fa43a5b88e66a64f2
cjwe <christajwest@gmail.com>
2021-10-21 16:36:58 +0200

    Updated pokemonList list for readability,  created loop to display Pokemon with height, highlighting small Pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,34 @@
 let pokemonList = [
-  {name: 'Bulbasaur', height: .7, types: ['grass','poison']},
-  {name: 'Charmander', height: .6, types: ['fire']},
-  {name: 'Squirtle', height: .5, types: ['water']},
-  {name: 'Pidgey', height: .3, types: ['flying','normal']},
-  {name: 'Weedle', height: .3, types: ['bug' , 'poison']},
+  {
+    name: 'Bulbasaur',
+    height: .7,
+    types: ['grass','poison']
+  },
+  {
+    name: 'Charmander',
+    height: .6,
+    types: ['fire']
+  },
+  {
+    name: 'Squirtle',
+    height: .5,
+    types: ['water']
+  },
+  {
+    name: 'Pidgey',
+    height: .3,
+    types: ['flying','normal']
+  },
+  {
+    name: 'Weedle',
+    height: .3,
+    types: ['bug' , 'poison']
+  },
 ];
+//lists Pokemon name with height & highlights small Pokemon
+for (let i=0; i < pokemonList.length; i++) {
+  document.write(pokemonList[i].name + ` (height: ${pokemonList[i].height}) `);
+  if (pokemonList[i].height <= .3)
+  document.write(' - Wow! That\'s a cute, little Pokemon! ');
+  document.write('<br>');
+  }
