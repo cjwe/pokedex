90523dbd0a3916470ac04766bc89fc6912ae5004
cjwe <christajwest@gmail.com>
2021-10-26 15:07:27 +0200

    IIFE created and therefore necessary changes - created .add and console.log, updated forEach loop, cleaned code

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,51 @@
-let pokemonList = [
-  {
-    name: 'Bulbasaur',
-    height: .7,
-    types: ['grass','poison']
-  },
-  {
-    name: 'Charmander',
-    height: .6,
-    types: ['fire']
-  },
-  {
-    name: 'Squirtle',
-    height: .5,
-    types: ['water']
-  },
-  {
-    name: 'Pidgey',
-    height: .3,
-    types: ['flying','normal']
-  },
-  {
-    name: 'Weedle',
-    height: .3,
-    types: ['bug' , 'poison']
-  },
-];
+//IIFE
+let pokemonRepository = (function() {
+  let pokemonList = [
+    {
+      name: 'Bulbasaur',
+      height: .7,
+      types: ['grass','poison']
+    },
+    {
+      name: 'Charmander',
+      height: .6,
+      types: ['fire']
+    },
+    {
+      name: 'Squirtle',
+      height: .5,
+      types: ['water']
+    },
+    {
+      name: 'Pidgey',
+      height: .3,
+      types: ['flying','normal']
+    },
+    {
+      name: 'Weedle',
+      height: .3,
+      types: ['bug' , 'poison']
+    },
+  ];
+
+  function add(pokemon) {
+    pokemonList.push(pokemon);
+  }
+
+  function getAll() {
+    return pokemonList;
+  }
+
+  return {
+    add: add,
+    getAll: getAll
+  };
+})();
+//adds new Pokemon to array
+pokemonRepository.add({name: 'Jigglypuff', height: .5, types: ['fariy' , 'normal']});
+
+//console logs complete list of Pokemon
+console.log(pokemonRepository.getAll() );
 
 //function to display Pokemon name with height & highlight large Pokemon
 function displayPokemon(pokemon) {
@@ -32,7 +53,7 @@ function displayPokemon(pokemon) {
   if (pokemon.height >= .6)
   document.write(' - Wow! That\'s big!');
   document.write('<br>');
-}
+  }
 
-//lists Pokemon name with height & highlights large Pokemon
-pokemonList.forEach(displayPokemon);
+//displays Pokemon name with height & highlights large Pokemon
+pokemonRepository.getAll().forEach(displayPokemon);
